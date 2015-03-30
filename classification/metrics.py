3cd4bbdb74724305e24672b07c541c1afff47d73
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-03-30 15:17:04 +0200

    SVM updated and use only hog

--- classification/metrics.py
+++ classification/metrics.py
@@ -6,7 +6,7 @@ def show_confusion_matrix(y_true, y_predicted, title=''):
     """
     Plot (and print) a confusion matrix from y_true and y_predicted
     """
-    confmat = confusion_matrix( y_true, y_predicted, title )
+    confmat = confusion_matrix( y_true, y_predicted )
     pl.figure(figsize=(10, 10))
     
     ax = pl.subplot(111)
@@ -19,6 +19,7 @@ def show_confusion_matrix(y_true, y_predicted, title=''):
             verticalalignment='center',
             fontsize=8)
 
+    ax.set_title( title )
     ax.set_xlabel('predicted label')
     ax.set_ylabel('true label')
     pl.colorbar(cmim, shrink=0.7, orientation='horizontal', pad=0.01)
@@ -27,4 +28,5 @@ def print_classification_report(y_true, y_pred, title=''):
     """
     Print a classification report
     """
-    print classification_report( y_true, y_pred, title )
+    print title
+    print classification_report( y_true, y_pred )
